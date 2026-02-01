1118259b4acc4614369bc1b2b088948c1407fc46
CYoJkoY <Cjy3430525946@outlook.com>
2026-02-01 18:06:24 +0800

    Initial commit

---
+++ extensions/progress_data.gd
@@ -0,0 +1,43 @@
+extends "res://singletons/progress_data.gd"
+
+var ncl_available_contents: Array = []
+
+# =========================== Extension =========================== #
+func _ready() -> void:
+    ncl_check_for_available_mods()
+
+    for ncl_available_content in ncl_available_contents:
+        ncl_available_content.add_resources()
+
+    RunData.reset()
+
+    if DebugService.generate_full_unlocked_save_file:
+        unlock_all()
+        save()
+    else:
+        load_game_file()
+        add_unlocked_by_default()
+
+    set_max_selectable_difficulty()
+
+# =========================== Custom =========================== #
+func ncl_check_for_available_mods() -> void:
+    var mod_datas: Dictionary = ModLoaderMod.get_mod_data_all()
+    for mod_data_id in mod_datas:
+        var mod_data: ModData = mod_datas[mod_data_id]
+        var dependencies: PoolStringArray = mod_data.manifest.dependencies
+        if !dependencies.has("Yoko-NewContentLoader"):
+            DebugService.log_data("[Skip] Can't find Yoko-NewContentLoader dependence in %s manifest" % [mod_data_id])
+            continue
+
+        var content_dir: String = mod_data.dir_path.plus_file("NewContentData.tres")
+        var mod_content: Resource = load(content_dir)
+
+        if mod_content == null:
+            DebugService.log_data("[Skip] Can't find NewContentData.tres file in %s folder" % [mod_data_id])
+            continue
+
+        DebugService.log_data("Found mod's NewContent.tres file, loading resources from: " + mod_data_id)
+        ncl_available_contents.append(mod_content)
+
+# =========================== Method =========================== #
---
+++ mod_main.gd
@@ -0,0 +1,13 @@
+extends Node
+
+const MYMODNAME_MOD_DIR := "Yoko-NewContentLoader"
+const MYMODNAME_LOG := "Yoko-NewContentLoader"
+
+var dir: String = ""
+var ext_dir: String = ""
+
+# =========================== Extension =========================== #
+func _init():
+    dir = ModLoaderMod.get_unpacked_dir().plus_file(MYMODNAME_MOD_DIR)
+    ext_dir = dir.plus_file("extensions")
+    ModLoaderMod.install_script_extension(ext_dir.plus_file("item_service.gd"))
