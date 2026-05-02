3d95754c1c21a95db9a9f4dcbdacbc07605a33fb
CYoJkoY <Cjy3430525946@outlook.com>
2026-05-02 23:53:48 +0800

    Update utils.gd

--- extensions/utils.gd
+++ extensions/utils.gd
@@ -33,6 +33,7 @@ func ncl_curse_effect_value(value: float, modifier: float, options: Dictionary =
 
 func ncl_curse_item(item_data: ItemParentData, player_index: int, turn_randomization_off: bool = false, min_modifier: float = 0.0) -> ItemParentData:
     var dlc_1: DLCData = ProgressData.get_dlc_data("abyssal_terrors")
+    if dlc_1 == null: return item_data
     return dlc_1.curse_item(item_data, player_index, turn_randomization_off, min_modifier)
 
 func ncl_curse_enemy(enemy: Enemy) -> void:
